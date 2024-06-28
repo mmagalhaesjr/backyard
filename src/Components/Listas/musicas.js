825c4f42264c7aa3b6907dbe5adcb1e98bda2d0a
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2024-06-28 12:00:57 -0300

    up

--- src/Components/Listas/musicas.js
+++ src/Components/Listas/musicas.js
@@ -5,7 +5,12 @@ import DuaLipaNewRules from '../../assets/musicas/mp3/DuaLipaNewRules.mp3'
 import BTSDynamite from '../../assets/musicas/mp3/BTSDynamite.mp3'
 import DuaLipaBreakMyHeart from '../../assets/musicas/mp3/DuaLipaBreakMyHeart.mp3'
 import BTSButter from '../../assets/musicas/mp3/BTSButter.mp3'
-
+import galopeira from '../../assets/musicas/mp3/ChitãozinhoXororóGalopeira.mp3'
+import FácilJotaQuest from '../../assets/musicas/mp3/FácilJotaQuest.mp3'
+import JovemDionísiotobem from '../../assets/musicas/mp3/JovemDionísiotobem.mp3'
+import kellykeybabababy from '../../assets/musicas/mp3/kellykeybabababy.mp3'
+import ritaleeervavenenosa  from '../../assets/musicas/mp3/ritaleeervavenenosa.mp3'
+import vamosFugir  from '../../assets/musicas/mp3/vamosFugir.mp3'
 
 export const ListaMusicas = [
     {nome:"Lift Me Up", musica:RihannaLiftMeUp},
@@ -15,5 +20,13 @@ export const ListaMusicas = [
     {nome:"Dynamite", musica:BTSDynamite},
     {nome:"Butter", musica:BTSButter},
     {nome:"Break My Heart", musica:DuaLipaBreakMyHeart},
+    {nome:"galopeira", musica:galopeira},
+    {nome:"Facíl", musica:FácilJotaQuest},
+    {nome:"To bem", musica:JovemDionísiotobem},
+    {nome:"Baba baby", musica:kellykeybabababy},
+    {nome:"Erva Venenosa", musica:ritaleeervavenenosa},
+    {nome:"Vamos fugir", musica:vamosFugir},
+
 
 ]
+
