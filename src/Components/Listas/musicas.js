9ebd375b8323dfcecda0ec2c27a1a6ff213c4723
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2024-12-27 12:05:11 -0300

    update musicas

--- src/Components/Listas/musicas.js
+++ src/Components/Listas/musicas.js
@@ -1,86 +1,58 @@
+import B24KMagic from '../../assets/musicas/ListaMusica/24KMagic.mp3'
+import BadRomance from '../../assets/musicas/ListaMusica/BadRomance.mp3'
+import baiana from '../../assets/musicas/ListaMusica/baiana.mp3'
+import CaliforniaDreamins from '../../assets/musicas/ListaMusica/CaliforniaDreamins.mp3'
+import ColdGin from '../../assets/musicas/ListaMusica/ColdGin.mp3'
+import Fantasy from '../../assets/musicas/ListaMusica/Fantasy.mp3'
+import GangstasParadise from '../../assets/musicas/ListaMusica/GangstasParadise.mp3'
+import GarotadeIpanema from '../../assets/musicas/ListaMusica/GarotadeIpanema.mp3'
+import HereComesTheSun from '../../assets/musicas/ListaMusica/HereComesTheSun.mp3'
+import ImComingOut from '../../assets/musicas/ListaMusica/ImComingOut.mp3'
+import IWantYouBack from '../../assets/musicas/ListaMusica/IWantYouBack.mp3'
+import LikeAVirgin from '../../assets/musicas/ListaMusica/LikeAVirgin.mp3'
+import LouBega from '../../assets/musicas/ListaMusica/LouBega.mp3'
+import Mambo5 from '../../assets/musicas/ListaMusica/Mambo5.mp3'
+import OhPrettyWoman from '../../assets/musicas/ListaMusica/OhPrettyWoman.mp3'
+import Paradise from '../../assets/musicas/ListaMusica/Paradise.mp3'
+import Pony from '../../assets/musicas/ListaMusica/Pony.mp3'
+import QueroTeEncontrar from '../../assets/musicas/ListaMusica/QueroTeEncontrar.mp3'
+import Romaria from '../../assets/musicas/ListaMusica/Romaria.mp3'
+import Satisfaction from '../../assets/musicas/ListaMusica/Satisfaction.mp3'
+import SexualHealing from '../../assets/musicas/ListaMusica/SexualHealing.mp3'
+import StayinAlive from '../../assets/musicas/ListaMusica/StayinAlive.mp3'
+import SweetChildOMine from '../../assets/musicas/ListaMusica/SweetChildOMine.mp3'
+import SweetDreams from '../../assets/musicas/ListaMusica/SweetDreams.mp3'
+import Toxic from '../../assets/musicas/ListaMusica/Toxic.mp3'
 
-import africa from '../../assets/musicas/ListaMusica/africa.mp3'
-import andarcomfe from '../../assets/musicas/ListaMusica/andarcomfe.mp3'
-import asuamaneira from '../../assets/musicas/ListaMusica/asuamaneira.mp3'
-import baba  from '../../assets/musicas/ListaMusica/baba.mp3'
-import billiejean  from '../../assets/musicas/ListaMusica/billiejean.mp3'
-import bohemianrhapsody  from '../../assets/musicas/ListaMusica/bohemianrhapsody.mp3'
-import cheiademanias  from '../../assets/musicas/ListaMusica/cheiademanias.mp3'
-import crazyinlove from '../../assets/musicas/ListaMusica/crazyinlove.mp3'
-import depois  from '../../assets/musicas/ListaMusica/depois.mp3'
-import domingodemanha from '../../assets/musicas/ListaMusica/domingodemanha.mp3'
-import dontstopbelieving from '../../assets/musicas/ListaMusica/dontstopbelieving.mp3'
-import eagleeyecherry from '../../assets/musicas/ListaMusica/eagleeyecherry.mp3'
-import esperandonajanela from '../../assets/musicas/ListaMusica/esperandonajanela.mp3'
-import faixaamarela from '../../assets/musicas/ListaMusica/faixaamarela.mp3'
-import glamurosa from '../../assets/musicas/ListaMusica/glamurosa.mp3'
-import hotelcalifornia from '../../assets/musicas/ListaMusica/hotelcalifornia.mp3'
-import infiel from '../../assets/musicas/ListaMusica/infiel.mp3'
-import iwillalwaysloveyou from '../../assets/musicas/ListaMusica/iwillalwaysloveyou.mp3'
-import jamming from '../../assets/musicas/ListaMusica/jamming.mp3'
-import livinonaprayer from '../../assets/musicas/ListaMusica/livinonaprayer.mp3'
-import luadecristal from '../../assets/musicas/ListaMusica/luadecristal.mp3'
-import maniadevoce from '../../assets/musicas/ListaMusica/maniadevoce.mp3'
-import materialgirl from '../../assets/musicas/ListaMusica/materialgirl.mp3'
-import meupedacodepecado from '../../assets/musicas/ListaMusica/meupedacodepecado.mp3'
-import osegundosol from '../../assets/musicas/ListaMusica/osegundosol.mp3'
-import peaches from '../../assets/musicas/ListaMusica/peaches.mp3'
-import penseemmim from '../../assets/musicas/ListaMusica/penseemmim.mp3'
-import pokerface from '../../assets/musicas/ListaMusica/pokerface.mp3'
-import ragatanga from '../../assets/musicas/ListaMusica/ragatanga.mp3'
-import ritchievalens from '../../assets/musicas/ListaMusica/ritchievalens.mp3'
-import rollinginthedeep from '../../assets/musicas/ListaMusica/rollinginthedeep.mp3'
-import secosemolhados from '../../assets/musicas/ListaMusica/secosemolhados.mp3'
-import sweetcaroline from '../../assets/musicas/ListaMusica/sweetcaroline.mp3'
-import tearsforfears from '../../assets/musicas/ListaMusica/tearsforfears.mp3'
-import tempoperdido from '../../assets/musicas/ListaMusica/tempoperdido.mp3'
-import tonemai from '../../assets/musicas/ListaMusica/tonemai.mp3'
-import vivalavida from '../../assets/musicas/ListaMusica/vivalavida.mp3'
-import waterloo from '../../assets/musicas/ListaMusica/waterloo.mp3'
-import wonderwall from '../../assets/musicas/ListaMusica/wonderwall.mp3'
-import xotedaalegria from '../../assets/musicas/ListaMusica/xotedaalegria.mp3'
+
+
 
 
 
 export const ListaMusicas = [
-    { id: 1, nome: "africa", musica: africa },
-    { id: 2, nome: "andar com fé", musica: andarcomfe },
-    { id: 3, nome: "a sua maneira", musica: asuamaneira },
-    { id: 4, nome: "baba", musica: baba },
-    { id: 5, nome: "billie jean", musica: billiejean },
-    { id: 6, nome: "bohemian rhapsody", musica: bohemianrhapsody },
-    { id: 7, nome: "cheia de manias", musica: cheiademanias },
-    { id: 8, nome: "crazy in love", musica: crazyinlove },
-    { id: 9, nome: "depois", musica: depois },
-    { id: 10, nome: "domingo de manhã", musica: domingodemanha },
-    { id: 11, nome: "don't stop believing", musica: dontstopbelieving },
-    { id: 12, nome: "eagle eye cherry", musica: eagleeyecherry },
-    { id: 13, nome: "esperando na janela", musica: esperandonajanela },
-    { id: 14, nome: "faixa amarela", musica: faixaamarela },
-    { id: 15, nome: "glamurosa", musica: glamurosa },
-    { id: 16, nome: "hotel california", musica: hotelcalifornia },
-    { id: 17, nome: "infiel", musica: infiel },
-    { id: 18, nome: "i will always love you", musica: iwillalwaysloveyou },
-    { id: 19, nome: "jamming", musica: jamming },
-    { id: 20, nome: "livin' on a prayer", musica: livinonaprayer },
-    { id: 21, nome: "lua de cristal", musica: luadecristal },
-    { id: 22, nome: "mania de você", musica: maniadevoce },
-    { id: 23, nome: "material girl", musica: materialgirl },
-    { id: 24, nome: "meu pedaço de pecado", musica: meupedacodepecado },
-    { id: 25, nome: "o segundo sol", musica: osegundosol },
-    { id: 26, nome: "peaches", musica: peaches },
-    { id: 27, nome: "pense em mim", musica: penseemmim },
-    { id: 28, nome: "poker face", musica: pokerface },
-    { id: 29, nome: "ragatanga", musica: ragatanga },
-    { id: 30, nome: "ritchie valens", musica: ritchievalens },
-    { id: 31, nome: "rolling in the deep", musica: rollinginthedeep },
-    { id: 32, nome: "secos e molhados", musica: secosemolhados },
-    { id: 33, nome: "sweet caroline", musica: sweetcaroline },
-    { id: 34, nome: "tears for fears", musica: tearsforfears },
-    { id: 35, nome: "tempo perdido", musica: tempoperdido },
-    { id: 36, nome: "tô nem aí", musica: tonemai },
-    { id: 37, nome: "viva la vida", musica: vivalavida },
-    { id: 38, nome: "waterloo", musica: waterloo },
-    { id: 39, nome: "wonderwall", musica: wonderwall },
-    { id: 40, nome: "xote da alegria", musica: xotedaalegria }
-];
+    { id: 1, nome: "B24KMagic", musica: B24KMagic },
+    { id: 2, nome: "Bad Romance", musica: BadRomance },
+    { id: 3, nome: "baianá", musica: baiana },
+    { id: 4, nome: "California Dreamins", musica: CaliforniaDreamins },
+    { id: 5, nome: "Cold Gin", musica: ColdGin },
+    { id: 6, nome: "Fantasy", musica: Fantasy },
+    { id: 7, nome: "Gangstas Paradise", musica: GangstasParadise },
+    { id: 8, nome: "Garotade Ipanema", musica: GarotadeIpanema },
+    { id: 9, nome: "Here Comes The Sun", musica: HereComesTheSun },
+    { id: 10, nome: "Im Coming Out", musica: ImComingOut },
+    { id: 11, nome: "I Want You Back", musica: IWantYouBack },
+    { id: 12, nome: "Like A Virgin", musica: LikeAVirgin },
+    { id: 13, nome: "Lou Bega", musica: LouBega },
+    { id: 14, nome: "Mambo 5", musica:Mambo5 },
+    { id: 15, nome: "Oh Pretty Woman", musica: OhPrettyWoman },
+    { id: 16, nome: "Paradise", musica: Paradise },
+    { id: 17, nome: "Pony", musica: Pony },
+    { id: 18, nome: "Quero Te Encontrar", musica: QueroTeEncontrar },
+    { id: 19, nome: "Romaria", musica: Romaria },
+    { id: 20, nome: "Satisfaction", musica: Satisfaction },
+    { id: 21, nome: "Sexual Healing", musica: SexualHealing },
+    { id: 22, nome: "Stayin Alive", musica: StayinAlive },
+    { id: 23, nome: "Sweet Child O Mine", musica: SweetChildOMine },
+    { id: 24, nome: "SweetDreams", musica: SweetDreams },
+    { id: 25, nome: "Toxic", musica: Toxic },
+]
